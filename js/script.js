49d20e01f6ec4b5f749e936016372d549fd910d3
SivtsovaT <66840978+SivtsovaT@users.noreply.github.com>
2021-03-07 10:11:24 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,28 +1,71 @@
-//header          
+function someFunc() {
+    counter();
+    add();
+   
+}
 
-    const AppHeader = () => {
-        let h1 = document.createElement('h1');
-        h1.className="app-header d-flex";
-        h1.innerHTML="Tatiana Sivtsova";
-        document.body.append(h1);
+let sum = 0;
+let count = document.getElementById("post_count");
+count.value = sum;
+
+function counter() {
+    sum +=1;
+    count.value = sum;
+    let elem = document.getElementById("post_count");
+    elem.innerHTML = sum;
+    let el =  document.querySelector('.red');
+    el.style.color = 'red';
+}
+
+function add() {
     
-        let h2 = document.createElement('h2');
-        h2.className="app-header d-flex";
-        h2.innerHTML ="5 записей, из них понравилось 0";
-        document.body.append(h2);
-    };
-    AppHeader();
+   
+      class PostList {
+        constructor(text, srcStar, altStar, srcTrash, altTrash, srcHeart, altHeart, parentSelector, ...classes) {
+            this.text = text;            
+            this.srcStar = srcStar;
+            this.altStar = altStar;
+            this.srcTrash = srcTrash;
+            this.altTrash = altTrash;
+            this.srcHeart = srcHeart;
+            this.altHeart = altHeart;          
+
+            this.parent = document.querySelector('.post-list');
+        }
     
-    //search-panel
-    
-    const SearchPanel = () => {
-        let input = document.createElement('input');
-        input.className="form-control search-input";
-        input.type = "text";
-        input.placeholder="Поиск по записям";
-        document.body.append(input);
-    
-    };
-     SearchPanel();
+        render() {
+            let answer = document.getElementById("answer");
+            const postList = document.createElement('div');
+            postList.innerHTML = `
+                                   
+            <div class ="post-list-item_text">${answer.value}</div>
+            <img class = 'star-solid' src=${this.srcStar} alt=${this.altStar} > 
+            <img class = 'trash-solid' src=${this.srcTrash} alt=${this.altTrash} >
+            <img class = 'heart-solid' src=${this.srcHeart} alt=${this.altHeart} >
+                        
+            `;
+            this.parent.append(postList);
+        }
+    }
+    new PostList(
+        
+        "hello",
+        "icons/star-solid.svg",
+        "star-solid",
+        "icons/trash-alt-solid.svg",
+        "trash-alt-solid",
+        "icons/heart-solid.svg",
+        "heart-solid",
+        
+        ".post-list"
+       
+    ).render(); 
+     
+  
+  }
 
 
+    
+    
+  
+    
